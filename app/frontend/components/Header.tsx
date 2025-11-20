44c034a15121a1c150183aa3d7686148e7773902
marbleheartg <197402001+marbleheartg@users.noreply.github.com>
2025-11-20 18:33:25 +0700

    refactor: reorder environment variables in .env.template, update package name to "flowers" in package.json, enhance Header component with connection button, and improve provider configuration for appkit integration.

--- app/frontend/components/Header.tsx
+++ app/frontend/components/Header.tsx
@@ -1,16 +1,19 @@
 import clsx from "clsx"
 // import Image from "next/image"
+import { useAppKit, useAppKitAccount } from "@reown/appkit/react"
 import { store } from "../../lib/store"
 
 const Header = () => {
   const { user } = store()
 
-  return (
-    <header className={clsx("fixed top-10 inset-x-0")}>
-      <div className={clsx("relative aspect-308/85 w-85 mx-auto", "text-center")}>
-        Header{/* <Image src={"/images/global/logo.svg"} fill alt="logo" /> */}
-      </div>
+  const { address, isConnected } = useAppKitAccount()
+  const { open } = useAppKit()
 
+  return (
+    <header className={clsx("fixed top-10 inset-x-8", "flex justify-between items-center")}>
+      <div className={clsx("text-xl p-1 px-2.5 pb-1.5 mb-0.5")}>Logo</div>
+
+      {/* <Image src={"/images/global/logo.svg"} fill alt="logo" /> */}
       {/* <Image
         src={user?.pfpUrl || "/images/global/user.svg"}
         width={19}
@@ -18,6 +21,10 @@ const Header = () => {
         alt="pfp"
         className={clsx("absolute top-0 right-0", "rounded-full")}
       /> */}
+
+      <button className={clsx("text-sm", "p-1 px-2.5", "border rounded-full", "cursor-pointer")} onClick={() => open({ view: "Connect" })}>
+        {isConnected ? address?.slice(0, 6) : "connect"}
+      </button>
     </header>
   )
 }
--- app/lib/providers/index.tsx
+++ app/lib/providers/index.tsx
@@ -1,20 +1,38 @@
 "use client"
 
 import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-connector"
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi"
+import { base } from "@reown/appkit/networks"
+import { createAppKit } from "@reown/appkit/react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { http } from "viem"
-import { base } from "viem/chains"
-import { createConfig, WagmiProvider } from "wagmi"
+import { CreateConnectorFn, WagmiProvider } from "wagmi"
 
-const wagmiConfig = createConfig({
-  chains: [base],
-  transports: {
-    [base.id]: http(),
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "YOUR_PROJECT_ID"
+
+const connectors: CreateConnectorFn[] = [miniAppConnector()]
+const wagmiAdapter = new WagmiAdapter({ networks: [base], projectId, ssr: false, connectors })
+
+createAppKit({
+  defaultNetwork: base,
+  adapters: [wagmiAdapter],
+  networks: [base],
+  projectId,
+  features: {
+    analytics: false,
+    swaps: false,
+    onramp: false,
+    allWallets: false,
+    // email: true,
+    socials: [
+      // "google", "x", "github", "discord", "apple", "facebook", "farcaster"
+    ],
+    // emailShowWallets: true,
   },
-  connectors: [miniAppConnector()],
+  enableNetworkSwitch: false,
+  enableWalletGuide: false,
 })
 
-const tanstackQueryClient = new QueryClient({
+const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 30,
@@ -26,8 +44,8 @@ const tanstackQueryClient = new QueryClient({
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={wagmiConfig}>
-      <QueryClientProvider client={tanstackQueryClient}>{children}</QueryClientProvider>
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   )
 }
