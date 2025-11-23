03c9c684086952167235eb98a5b744a4b50f2389
marbleheartg <197402001+marbleheartg@users.noreply.github.com>
2025-11-23 19:55:24 +0700

    feat: enhance Home component with automatic mini app addition upon successful contract interaction; refactor useEffect for improved state management.

--- app/frontend/pages/Home.tsx
+++ app/frontend/pages/Home.tsx
@@ -1,14 +1,25 @@
 import { CA } from "@/lib/constants"
 import { useWriteContractLfg } from "@/lib/contract"
+import { store } from "@/lib/store"
+import sdk from "@farcaster/miniapp-sdk"
 import clsx from "clsx"
+import { useEffect } from "react"
 import { base } from "viem/chains"
 import { useConnect, useSwitchChain, useWaitForTransactionReceipt } from "wagmi"
 
 export default function Home() {
+  const { connect, connectors } = useConnect()
+  const { switchChain } = useSwitchChain()
+
   const { data: hash, writeContract } = useWriteContractLfg()
   const { isLoading, isSuccess } = useWaitForTransactionReceipt({ hash })
-  const { switchChain } = useSwitchChain()
-  const { connect, connectors } = useConnect()
+
+  useEffect(() => {
+    if (!isSuccess) return
+    if (store.getState()?.client?.added) return
+
+    sdk.actions.addMiniApp().catch(() => {})
+  }, [isSuccess])
 
   return (
     <main
